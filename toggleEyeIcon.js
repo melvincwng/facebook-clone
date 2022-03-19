ab6d82127595fec791c957dadbd1ea6f8a104be5
melvincwng <melvinng93@outlook.com>
2022-03-20 02:51:30 +0800

    Refine toggleEyeIcon.js script logic to make it compatible for mobile phones

--- toggleEyeIcon.js
+++ toggleEyeIcon.js
@@ -2,20 +2,19 @@ function initializeScript() {
   const toggleEyeIcon = document.querySelector("#toggleEyeIcon");
   const passwordField = document.querySelector("#password_field");
 
-  passwordField.addEventListener("keypress", function () {
-    toggleEyeIcon.style.display = "block";
-  });
-
   // Take note for this block, do not use "keypress/keyup/keydown" event ---> it will cause lags in getting the value of passwordField
   // E.g. you type in '1' --> at first, this.value = "" --> then you type in '12', then it shows '1' --> it will always be lagging by one value
   // But if you use "input" event --> there's no lag & you get the latest value of the password field after keying in
-  // Reference: https://stackoverflow.com/questions/9906885/detect-backspace-and-del-on-input-event
+  // Also 'keypress' events do not work in mobile --> But 'input' and 'change' events work in mobile
+  // Reference: https://stackoverflow.com/questions/9906885/detect-backspace-and-del-on-input-event && https://stackoverflow.com/questions/41234395/how-do-i-listen-for-input-events-on-mobile-browsers
 
   passwordField.addEventListener("input", function (event) {
     const inputType = event.inputType;
     const passwordFieldEmpty = !this.value;
     if (inputType === "deleteContentBackward" && passwordFieldEmpty) {
       toggleEyeIcon.style.display = "none";
+    } else {
+      toggleEyeIcon.style.display = "block";
     }
   });
 
